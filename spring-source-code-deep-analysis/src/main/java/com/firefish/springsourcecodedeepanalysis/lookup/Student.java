57eabf44f3b123a3256ccd240fd1f913222342f7
1420193230@qq.com <1420193230@qq.com>
2022-09-03 10:02:24 +0800

    lookup-method 是指"bean的替换"

---
+++ spring-source-code-deep-analysis/src/main/java/com/firefish/springsourcecodedeepanalysis/lookup/Student.java
@@ -0,0 +1,15 @@
+package com.firefish.springsourcecodedeepanalysis.lookup;
+
+/**
+ * 功能: 描述.
+ *
+ * @author Fire Fish
+ * @since 2022-09-03 9:54 AM
+ */
+public class Student extends User {
+
+	@Override
+	public void showMe() {
+		System.out.println("i am student");
+	}
+}
---
+++ spring-source-code-deep-analysis/src/main/java/com/firefish/springsourcecodedeepanalysis/lookup/User.java
@@ -0,0 +1,7 @@
+package com.firefish.springsourcecodedeepanalysis.lookup;
+
+public class User {
+	public void showMe() {
+		System.out.println("i'm user.");
+	}
+}
