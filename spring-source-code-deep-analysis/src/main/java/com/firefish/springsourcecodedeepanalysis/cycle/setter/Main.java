c3a378d75abd527fff404a9374beefe2e449f633
1420193230@qq.com <1420193230@qq.com>
2022-09-09 10:01:29 +0800

    "循环依赖和三级缓存"源码的测试代码

---
+++ spring-source-code-deep-analysis/src/main/java/com/firefish/springsourcecodedeepanalysis/cycle/setter/Main.java
@@ -0,0 +1,15 @@
+package com.firefish.springsourcecodedeepanalysis.cycle.setter;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+public class Main {
+	public static void main(String[] args) {
+		XmlBeanFactory context = new XmlBeanFactory(new ClassPathResource("com/firefish/springsourcecodedeepanalysis/cycle/setter/application.xml"));
+//		ApplicationContext context = new ClassPathXmlApplicationContext(
+//                "com/firefish/springsourcecodedeepanalysis/cycle/setter/application.xml");
+		System.out.println(context.getBean("bean_3"));
+	}
+}
